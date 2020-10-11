9c1496e37faaa42f103b5bedede04c4082a0f59b
mahadevananair1 <mahadevananair@gmail.com>
2020-10-11 18:02:49 +0530

    education homepage only  completed quick push nothing major

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import calendar
+import datetime
 
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
@@ -7,6 +9,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from datetime import date
 from helpers import apology, login_required, lookup, usd
 
 # Configure application
@@ -38,6 +41,14 @@ db = SQL("sqlite:///finance.db")
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
     raise RuntimeError("API_KEY not set")
+
+def add_months(sourcedate, months):
+    month = sourcedate.month - 1 + months
+    year = sourcedate.year + month // 12
+    month = month % 12 + 1
+    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
+    return datetime.date(year, month, day)
+
 
 
 @app.route("/")
@@ -232,23 +243,85 @@ def personalupdate(pid):
         return apology("UNDApakuru") 
 
 
-@app.route("/education")
+@app.route("/education",methods=["GET", "POST"])
 @login_required
 def education():
     """Show portfolio of educational spendings"""
-    return apology("TODO")
+    if request.method == "POST":
+        epurpose = request.form.get("epurpose")
+        amount = request.form.get("amount")
+        period = request.form.get("period")
+        try:
+            amount = int(amount)
+            period = int(period)
+        except:
+            return apology("enter a proper value")
+        if not epurpose:
+            return apology("Missing  commodity!")
+        elif not amount:
+            return apology("Missing number of shares!")
+        elif int(amount)<= 0:
+            return apology("enter a proper value")    
+        else:  
+            rows = db.execute("SELECT cash FROM users WHERE id=:id",id=session["user_id"])
+            cash = rows[0]["cash"]
+            updated_cash = cash - amount 
+            if updated_cash <0:
+                return apology("cant afford")
+            today = date.today() 
+            duedate = add_months(today,period)   
+            db.execute("UPDATE users SET cash=:updated_cash WHERE id=:id",updated_cash=updated_cash,id=session["user_id"])
+            db.execute("INSERT INTO education (user_id,epurpose,amount,period,duedate) VALUES (:user_id,:epurpose,:amount,:period,:duedate)",user_id=session["user_id"],epurpose=epurpose,amount=amount,period=period,duedate=duedate)
+            flash("Educated!!")
+            return redirect("/education") 
+    else:
+            erows = db.execute("""SELECT eid,epurpose,amount,period,datestamp,duedate 
+            FROM education
+            WHERE user_id = :user_id
+            ORDER BY eid;
+            """,user_id=session['user_id'])
+            espendings = []
+            grand_etotal =0
+            for erow in erows:
+                espendings.append({
+                    "eid": erow["eid"],
+                    "epurpose": erow["epurpose"],
+                    "amount": erow["amount"],
+                    "period": erow["period"],  
+                    "datestamp": erow["datestamp"], 
+                    "duedate": erow["duedate"],   
+                }) 
+                grand_etotal = grand_etotal + erow["amount"]
+            cash = db.execute("SELECT cash FROM users WHERE id=:user_id",user_id=session["user_id"]) 
+            cash = cash[0]["cash"]   
+            return render_template("education.html",espendings=espendings, cash =usd(cash), grand_etotal=usd(grand_etotal))
 
-@app.route("/education/add")
-@login_required
-def educationadd():
-    """Show portfolio of personal spendings"""
-    return apology("TODO")   
 
-@app.route("/education/delete")
+@app.route("/education/delete/<int:eid>")
 @login_required
-def educationdelete():
+def educationdelete(eid):
     """Show portfolio of personal spendings"""
-    return apology("TODO") 
+    rows = db.execute("SELECT cash FROM users WHERE id=:id",id=session["user_id"])
+    erowsD = db.execute("SELECT amount,datestamp FROM education WHERE eid=:eid",eid=eid)
+    cash = rows[0]["cash"]
+    amount = erowsD[0]["amount"]
+    datestamplist= erowsD[0]["datestamp"].split('-')
+    datestamp = datetime.date(int(datestamplist[0]), int(datestamplist[1]), int(datestamplist[2]))
+    today = date.today()
+    print(cash)
+    print(datestamp)
+    if (datestamp.year - today.year) * 12 + (datestamp.month - today.month) > 0:
+        updated_cash = cash + 1
+        print("date of payment is further than today")
+        print((datestamp.year - today.year) * 12 + (datestamp.month - today.month))
+    else:
+        updated_cash = cash + amount
+        print("date of payment due is behind today")
+        print((datestamp.year - today.year) * 12 + (datestamp.month - today.month))
+    db.execute("UPDATE users SET cash=:updated_cash WHERE id=:id",updated_cash=updated_cash,id=session["user_id"])
+    db.execute("DELETE FROM education WHERE eid =:eid ",eid=eid)
+    flash("Deleted!!")
+    return redirect("/education") 
 
 @app.route("/education/update")
 @login_required
